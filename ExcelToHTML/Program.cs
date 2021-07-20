2add7a5303aa48405a43d6fc78b047b8b8dcf93a
FofLof <ethan_see@hotmail.com>
2021-07-20 14:54:24 -0700

    tried something else

--- ExcelToHTML/Program.cs
+++ ExcelToHTML/Program.cs
@@ -15,68 +15,82 @@ namespace ExcelToHTML
         static void Main(string[] args)
 
         {
-            var tableThree = new Aspose.Cells.Workbook("2019Table3.xlsx");
+            /*var tableThree = new Aspose.Cells.Workbook("2019Table3.xlsx");
             //save XLS as HTML
-            tableThree.Save("2019Table3.html", Aspose.Cells.SaveFormat.Auto);
-            /*SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("2019Table3.xlsx", false);
-            spreadsheetDocument.ChangeDocumentType((SpreadsheetDocumentType)WordprocessingDocumentType.Document);
-            string html =
-        @"<!DOCTYPE html>
-<html>
-<body>
-	<h1>This is <b>bold</b> heading</h1>
-	<p>" + spreadsheetDocument + "paragraph</p>" +
-	"<h2>This is <i>italic</i> heading</h2>" +
-	"<h2>This is new heading</h2>" +
-"</body>" +
-"</html> ";
+            Aspose.Cells.HtmlSaveOptions htmlSaveOptions = new Aspose.Cells.HtmlSaveOptions();
+            tableThree.Save("fascd", );
+            //tableThree.Save("2019Table3(1).html");*/
 
-            WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
-            workbookPart.Workbook = new Workbook();
+
+
+
             var doc = new HtmlDocument();
+            /*doc.Load(path);
+            doc.Save("test.html");*/
+
+            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("2019Table3.xlsx", false);
+
+            spreadsheetDocument.ChangeDocumentType((SpreadsheetDocumentType)WordprocessingDocumentType.Document);
+
+
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                workbookPart.Workbook = new Workbook();
+            /*var doc = new HtmlDocument();
 
             doc.Load(html);
-            doc.Save("2019Table3.html"); 
+            doc.Save("2019Table3.html");
 
             var newDoc = new HtmlDocument();
             newDoc.DetectEncodingAndLoad("2019Table3.html");
-            var node = newDoc.DocumentNode.SelectSingleNode("//body");*/
-
-
-
-
-
-            /*WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-            SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
-            string text;
-            foreach (Row r in sheetData.Elements<Row>())
-            {
-                foreach (Cell c in r.Elements<Cell>())
-                {
-                    text = c.CellValue.Text;
-                    Console.Write(text + " ");
-                }
-            }
-
-            FileStream sw = new FileStream("2019Table3.html", FileMode.Create);
-            //htmlDoc.Save(sw);
-            sw.Close();
+            var node = newDoc.DocumentNode.SelectSingleNode("//body");
+*/
 
 
             string html = "<head>";
-            string style = "text-align:center";
-
             html += "<title>Page Title</title>";
-            html += "<style>" + style + "</style>";
+
             html += "</head><body>";
-            html +=
-            html += "</body>";
+            WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+            SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+            string dataText;
+            foreach (Row r in sheetData.Elements<Row>())
+              {
+                 foreach (Cell c in r.Elements<Cell>())
+                 {
+                    dataText = c.CellValue.Text;
+                    html += dataText;
+                 }
+             }
+              
+                html += "</body>";
+
+                doc.LoadHtml(html);
+                using (FileStream fs = new FileStream("afsc.html", FileMode.Create))
+                using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8) { AutoFlush = true })
+                {
+                    doc.Save(sw);
+
+                /*FileStream sw = new FileStream("2019Table3.html", FileMode.Create);
+                //htmlDoc.Save(sw);
+                sw.Close();
 
 
-            var doc = new HtmlDocument();
-            doc.Load("2019Table2.xls");
+                string html = "<head>";
+                string style = "text-align:center";
+
+                html += "<title>Page Title</title>";
+                html += "<style>" + style + "</style>";
+                html += "</head><body>";
+                html +=
+                html += "</body>";
+
+
+                var doc = new HtmlDocument();
+                doc.Load("2019Table2.xls");
 */
 
+
+            }
         }
     }
 }
