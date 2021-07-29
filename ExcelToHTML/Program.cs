ec2fffb869e160fb06438922bc1034e137ebacca
FofLof <ethan_see@hotmail.com>
2021-07-29 01:37:40 -0700

    No longer file specific for column, can do multiple sheets, var for fileName, and cleaner

--- ExcelToHTML/Program.cs
+++ ExcelToHTML/Program.cs
@@ -7,20 +7,14 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using Aspose.Cells;
+
 //using Microsoft.Office.Interop.Excel;
 
 namespace ExcelToHTML
 {
     class Program
     {
-        public static string GetColumnName(string cellReference)
-        {
-            // Create a regular expression to match the column name portion of the cell name.
-            Regex regex = new Regex("[A-Za-z]+");
-            Match match = regex.Match(cellReference);
-            return match.Value;
-        }
-
         public static string GetCellValue(string fileName,
         string sheetName,
         string addressName)
@@ -51,7 +45,7 @@ namespace ExcelToHTML
 
                 // Use its Worksheet property to get a reference to the cell 
                 // whose address matches the address you supplied.
-                Cell theCell = wsPart.Worksheet.Descendants<Cell>().
+                DocumentFormat.OpenXml.Spreadsheet.Cell theCell = wsPart.Worksheet.Descendants<DocumentFormat.OpenXml.Spreadsheet.Cell>().
                   Where(c => c.CellReference == addressName).FirstOrDefault();
 
                 // If the cell does not exist, return an empty string.
@@ -108,97 +102,182 @@ namespace ExcelToHTML
             }
             return value;
         }
+
+        public static Sheets GetAllWorksheets(string fileName)
+        {
+            Sheets theSheets = null;
+
+            using (SpreadsheetDocument document =
+                SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart wbPart = document.WorkbookPart;
+                theSheets = wbPart.Workbook.Sheets;
+            }
+            return theSheets;
+        }
+
+        //Should I make this receive user input for the file name?
+        public static string fileName = "2019Table3.xlsx";
+        public static string[] sheetNameArray;
         static void Main(string[] args)
 
         {
-            /*var tableThree = new Aspose.Cells.Workbook("2019Table3.xlsx");
-            //save XLS as HTML
-            Aspose.Cells.HtmlSaveOptions htmlSaveOptions = new Aspose.Cells.HtmlSaveOptions();
-            tableThree.Save("fascd", );
-            //tableThree.Save("2019Table3(1).html");*/
-
-            var doc = new HtmlDocument();
-
-            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open("2019Table3.xlsx", false);
-
-            spreadsheetDocument.ChangeDocumentType((SpreadsheetDocumentType)WordprocessingDocumentType.Document);
-
+            SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false);
 
             WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
-            workbookPart.Workbook = new Workbook();
+            workbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
 
-            string html = "<head>";
-            html += "<title>Page Title</title>";
+            Aspose.Cells.Workbook wb = new Aspose.Cells.Workbook(fileName);
+            //Access the first worksheet
+            Aspose.Cells.Worksheet ws = wb.Worksheets[0];
+            //Access last cell inside the worksheet
+            Aspose.Cells.Cell cell = ws.Cells.LastCell;
+            //Number of rows inside the worksheet
+            int rowCount = cell.Row + 1;
+            int n = 0;
+            int sheetPos = 0; 
+            var sheetNames = GetAllWorksheets(fileName);
 
-            html += "</head><body>";
-            html += "<p>";
-            WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-            SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+            foreach (Sheet item in sheetNames) //probably some method/way somewhere to get # of sheets
+            {
+                n += 1;
+            }
+            sheetNameArray = new string[n];
+
+            foreach (Sheet item in sheetNames)
+            {
+                sheetNameArray[sheetPos] = item.Name;
+                sheetPos += 1;
+            }
+            var doc = new HtmlDocument();
 
             string dataText;
+            string[] columnLetter = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" }; //gotta be a better way then just putting the letter
+            //Seems like it still works if i do it like this; tried making a new array that would only take the necessary letters based off
+            //number of columns but that didnt work cause column count ended up at 0 even using different libraries so fjicsdjjviopsk
+            IEnumerable<WorksheetPart> worksheetPart = workbookPart.WorksheetParts;
 
-                String[] columnLetter = { "A", "B", "C", "D", "E", "F", "G", "I" }; //gotta be a better way then just putting the letter
-
-            /*foreach (Row r in sheetData.Elements<Row>())
+            //Probably atrocious to read this sorry
+            for (int i = 0; i < n; i++) //loop thru sheet # making an html file per
             {
-                foreach (Cell c in r.Elements<Cell>())
+                string html = "<head>";
+                html += "<title>Page Title</title>";
+                html += "</head><body>";
+                html += "<p>";
+                html += "<table>";
+                for (int j = 1; j <= rowCount; j++) //looping thru each row and column and adding cells as needed
                 {
-                    foreach (string l in columnLetter)
-                    {
-                        for (int i = 1; i <= 38; i++) //there's gotta be a more efficient way then to just go count the number of rows why am I so bad at coding 
-                        {
-                            if (c.DataType != null)
-                            {
-                                dataText = GetCellValue("2019Table3.xlsx", "SUM_3_06_2019", l + i.ToString());
-                                html += "   ";
-                                html += dataText;
-                            }
-                            else
-                            {
-                                html += "\r\n";
-                            }
-                        }
-
-                    }
-                }
-            }*/
-
-            html += "<table>";
-            html += "Table 3. Incidence rates1 of nonfatal occupational injuries and illnesses by industry sector and employment size, California, 2019";
-                for (int i = 3; i <= 38; i++) {
                     html += "<tr>";
-                    foreach (string l in columnLetter)
+                    foreach (string l in columnLetter) 
                     {
                         html += "<td rowspan = 1 colspan = 0>";
-                       
-                        if (GetCellValue("2019Table3.xlsx", "SUM_3_06_2019", l + i.ToString()) != null) 
+
+                        if (GetCellValue(fileName, sheetNameArray[i], l + j.ToString()) != null) //if cell not empty add cell
                         {
-                     
-                            dataText = GetCellValue("2019Table3.xlsx", "SUM_3_06_2019", l + i.ToString());
+                            dataText = GetCellValue(fileName, sheetNameArray[i], l + j.ToString());
                             html += " ";
                             html += dataText;
-                            html +=  "</td>";
+                            html += "</td>";
                         }
                         else
                         {
-                            
                             html += "<br>";
-                        html += "</td>";
+                            html += "</td>";
                         }
 
-                    }
-                    html += "</tr>";
+                    } 
                 }
+                html += "</tr>";
                 html += "</table>";
                 html += "<p>";
                 html += "</body>";
-
                 doc.LoadHtml(html);
-                using (FileStream fs = new FileStream("afas.html", FileMode.Create))
+                string htmlName = "Table";
+                Console.WriteLine("Making a file");
+                using (FileStream fs = new FileStream(htmlName + (i + 1).ToString() + ".html", FileMode.Create))
                 using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8) { AutoFlush = true })
                 {
                     doc.Save(sw);
                 }
+                html = string.Empty;
             }
         }
     }
+}
+
+//Failed ideas below may be useful later
+
+
+
+
+
+/*foreach (WorksheetPart WSP in worksheetPart)
+            {
+                //find sheet data
+                IEnumerable<SheetData> sheetData = WSP.Worksheet.Elements<SheetData>();
+                // Iterate through every sheet inside Excel sheet
+                foreach (SheetData SD in sheetData)
+                {
+                    IEnumerable<DocumentFormat.OpenXml.Spreadsheet.Row> row = SD.Elements<Row>(); // Get the row IEnumerator
+                    rowCount = row.Count(); ; // Will give you the count of rows
+                }
+            }*/
+
+
+//WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+//SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+/*foreach (Row r in sheetData.Elements<Row>())
+{
+    foreach (Cell c in r.Elements<Cell>())
+    {
+        foreach (string l in columnLetter)
+        {
+            for (int i = 1; i <= 38; i++) //there's gotta be a more efficient way then to just go count the number of rows why am I so bad at coding 
+            {
+                if (c.DataType != null)
+                {
+                    dataText = GetCellValue("2019Table3.xlsx", "SUM_3_06_2019", l + i.ToString());
+                    html += "   ";
+                    html += dataText;
+                }
+                else
+                {
+                    html += "\r\n";
+                }
+            }
+        }
+    }
+}*/
+
+/*foreach (WorksheetPart WSP in worksheetPart)
+{
+    //find sheet data
+    IEnumerable<SheetData> sheetData = WSP.Worksheet.Elements<SheetData>();
+    // Iterate through every sheet inside Excel sheet
+    foreach (SheetData SD in sheetData)
+    {
+        IEnumerable<DocumentFormat.OpenXml.Spreadsheet.Row> row = SD.Elements<Row>(); // Get the row IEnumerator
+        rowCount = row.Count(); ; // Will give you the count of rows
+    }
+}*/
+
+/*didnt work column count always ended up at 0
+
+            string[] columnsInDoc = new string[columnCount];
+            for (int i = 0; i < columnCount; i++)
+            {
+                columnLetter[i] = columnsInDoc[i];
+            }
+            Console.WriteLine(columnCount);
+
+            foreach (string t in columnsInDoc)
+            {
+                Console.WriteLine(t.ToString());
+            }*/
+
+/*foreach (Sheet item in sheetNames) //didnt work lol
+            {
+                sheetNameArray = new string[n];
+                sheetNameArray[n - 1] = item.Name;
+                n += 1;
+            }*/
